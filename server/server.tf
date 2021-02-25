240ad6331090cdd3adf79843658cd2147c57c6cc
vsakruti <42459450+vsakruti@users.noreply.github.com>
2021-02-25 11:43:56 -0800

    Update server.tf

--- server/server.tf
+++ server/server.tf
@@ -1,9 +1,4 @@
 
-provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.region
-}
 
 variable "servers" {
   description = "Map of server types to configuration"
