e5eab13c154462015be5d1b7bf15d964bc0c6fb6
vsakruti <42459450+vsakruti@users.noreply.github.com>
2021-02-25 11:23:36 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+module "keypair" {
+  source  = "mitchellh/dynamic-keys/aws"
+  version = "2.0.0"
+  path    = "${path.root}/keys"
+  name    = "${var.identity}-key-foreach"
+}
+
+module "server" {
+  source       = "./server"
+  identity     = var.identity
+  access_key   = var.access_key
+  secret_key   = var.secret_key
+  student_name = var.student_name
+  student_id   = var.student_id
+  key_name     = module.keypair.key_name
+  private_key  = module.keypair.private_key_pem
+}
